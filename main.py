0a645c14c1a54e155e05097ac3891a238e65a802
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2023-12-15 15:01:47 -0300

    creating a new app page

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
 
 
 GUI = Builder.load_file('main.kv')
+
+
+class Homepage(Screen):
+    pass
 
 
 class MainApp(App):
