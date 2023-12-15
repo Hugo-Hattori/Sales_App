2328a59cbf6408124bbf874b725960eb1103da11
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2023-12-15 15:00:20 -0300

    first kv file

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 from kivy.app import App
+from kivy.lang import Builder
+
+
+GUI = Builder.load_file('main.kv')
+
+
+class MainApp(App):
+
+    def build(self):
+        return GUI
+
+
+MainApp().run()
