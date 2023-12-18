1e4595679c02ffe58f6b46ca7d84f3f3fd1ac9ad
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2023-12-18 13:56:55 -0300

    Creating LabelButton and ImageButton

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen
-
-
-class HomePage(Screen):
-    pass
-
-
-class ConfigPage(Screen):
-    pass
+from telas import *
+from botoes import *
 
 
 GUI = Builder.load_file('main.kv')
---
+++ telas.py
@@ -0,0 +1,9 @@
+from kivy.uix.screenmanager import Screen
+
+
+class HomePage(Screen):
+    pass
+
+
+class ConfigPage(Screen):
+    pass
