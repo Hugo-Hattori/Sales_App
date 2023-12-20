f1bebe6a90e4ea2dcf7b736a9ba806def28fc1c1
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2023-12-20 14:36:25 -0300

    Adding Sales Banner

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.lang import Builder
 from telas import *
 from botoes import *
 import requests
+from bannervenda import BannerVenda
 
 
 GUI = Builder.load_file('main.kv')
@@ -28,7 +29,13 @@ class MainApp(App):
             print(requisicao_dic['vendas'])
             vendas = requisicao_dic['vendas'][1:]
             for venda in vendas:
-                print(venda)
+                banner = BannerVenda(cliente=venda['cliente'], foto_cliente=venda['foto_cliente'],
+                                     produto=venda['produto'], foto_produto=venda['foto_produto'],
+                                     preco=venda['preco'], data=venda['data'], unidade=venda['unidade'],
+                                     quantidade=venda['quantidade'])
+                pagina_homepage = self.root.ids["homepage"]
+                lista_vendas = pagina_homepage.ids["lista_vendas"]
+                lista_vendas.add_widget(banner) #adicionar um item na lista de vendas
         except:
             pass
 
