0bf9ab326e60ade7b1f803dabc4b21c892c92e58
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2023-12-23 17:40:37 -0300

    Displaying error message to user

--- myFirebase.py
+++ myFirebase.py
@@ -1,4 +1,5 @@
 import requests
+from kivy.app import App
 
 
 class MyFirebase():
@@ -8,10 +9,19 @@ class MyFirebase():
         link = f"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={self.API_KEY}"
         print(email, senha)
         info = {"email": email,
-                "senha": senha,
+                "password": senha,
                 "returnSecureToken": True}
         requisicao = requests.post(link, data=info)
         requisicao_dic = requisicao.json()
+
+        if requisicao.ok:
+            print("Usuário Criado")
+        else:
+            mensagem_erro = requisicao_dic["error"]["message"]
+            meu_aplicativo = App.get_running_app() #retorna a classe MainApp
+            pagina_login = meu_aplicativo.root.ids["loginpage"]
+            pagina_login.ids["mensagem_login"].text = mensagem_erro
+            pagina_login.ids["mensagem_login"].color = (1, 0, 0, 1)
         print(requisicao_dic)
 
     def fazer_login(self, email, senha):
